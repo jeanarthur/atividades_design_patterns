190b17ee4e1f0a386a272ed21582d38d01d36cf1
jeanarthur <jean.arthursa@gmail.com>
2025-08-31 21:33:53 -0300

    adiciona menu para selecionar tipo notificacao

--- src/Atividade_04/Exercicio_01/Main.java
+++ src/Atividade_04/Exercicio_01/Main.java
@@ -1,20 +1,49 @@
 package Atividade_04.Exercicio_01;
 
+import java.util.Scanner;
+
 public class Main {
-    
+
     public static void main(String[] args) {
-        
-        ServicoNotificacao servicoEmail = new ServicoEmail();
-        servicoEmail.notificar("Boa noite, segue am anexo... Atenciosamente");
-        System.err.println();
 
-        ServicoNotificacao servicoSms = new ServicoSMS();
-        servicoSms.notificar("Internet sem limites: TIM Ultrafibra 500...");
-        System.err.println();
+        while (true) {
+            System.err.println("+-----------------------+");
+            System.err.println("| 1 - Enviar Email      |");
+            System.err.println("| 2 - Enviar SMS        |");
+            System.err.println("| 3 - Enviar Push       |");
+            System.err.println("| 0 - Sair              |");
+            System.err.println("+-----------------------+");
+            System.err.print("Escolha uma opção: ");
+            Scanner scanner = new Scanner(System.in);
 
-        ServicoNotificacao servicoPush = new ServicoPush();
-        servicoPush.notificar("É hora de beber agua!");
+            int opcao = scanner.nextInt();
+            scanner.nextLine();
+            if (opcao == 0) {
+                System.err.println("Saindo...");
+                scanner.close();
+                break;
+            }
 
+            ServicoNotificacao servico;
+            switch (opcao) {
+                case 1:
+                    servico = new ServicoEmail();
+                    break;
+                case 2:
+                    servico = new ServicoSMS();
+                    break;
+                case 3:
+                    servico = new ServicoPush();
+                    break;
+                default:
+                    System.err.println("Opção inválida!");
+                    continue;
+            }
+
+            System.err.print("Digite a mensagem: ");
+            String mensagem = scanner.nextLine();
+            servico.notificar(mensagem);
+        }
     }
 
 }
